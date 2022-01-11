58109f0cf636b69206944f9ef1ca9d5365dad871
Pradeep <webreinvent@gmail.com>
2022-01-11 12:53:52 +0530

    updated: nuxt 3 + oruga + bulma

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+
+    css: [
+        // SCSS file in the project
+        '@/assets/scss/build.scss',
+        //'@/assets/scss/bulma.scss',
+        //'@/assets/scss/oruga.scss',
+        //'@oruga-ui/oruga-next/src/scss/oruga-full-vars.scss',
+        //'@oruga-ui/theme-bulma/dist/bulma.css',
+    ],
+
+})
